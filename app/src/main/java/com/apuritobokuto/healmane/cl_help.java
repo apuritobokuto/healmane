729df4060c77c4d03c6f4672ad8efaca810b7221
ryusato <ryusato.kochi@gmail.com>
2016-12-16 15:43:16 +0900

    ファイル名変更2

--- app/src/main/java/com/apuritobokuto/healmane/cl_help.java
+++ app/src/main/java/com/apuritobokuto/healmane/cl_help.java
@@ -1,8 +1,20 @@
 package com.apuritobokuto.healmane;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by RyuSato on 2016/12/16.
  */
 
-public class cl_help {
+public class cl_help extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+
+
 }
