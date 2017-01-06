d30e42582be80a65a0789009f32c10155ace3bce
ykunihiro <Yuto@YutoMac.local>
2017-01-06 16:02:48 +0900

    グラフ表示できたよ

--- app/src/main/java/com/apuritobokuto/healmane/cl_healthManage.java
+++ app/src/main/java/com/apuritobokuto/healmane/cl_healthManage.java
@@ -1,23 +1,54 @@
 package com.apuritobokuto.healmane;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import java.lang.reflect.AccessibleObject;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
-/**
- * Created by Ryu on 2016/11/16.
- */
+import java.util.ArrayList;
 
 public class cl_healthManage extends Activity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
-        }
+
+        LineChart lineChart = (LineChart)findViewById(R.id.chart1);
+
+        //グラフ用データ
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry(60f,0));
+        entries.add(new Entry(50f,1));
+        entries.add(new Entry(58f,2));
+        //entries.add(new Entry(60f,3));
+        //entries.add(new Entry(65f,4));
+        //entries.add(new Entry(80f,5));
+        //entries.add(new Entry(78f,6));
+
+        //データをセット
+        LineDataSet dataSet = new LineDataSet(entries,"weight");
+
+        //ラベル
+        String[] labels = {"2015","2016","2017"};
+
+        //LineDataインスタンス生成
+        LineData data = new LineData(labels,dataSet);
+
+        //LineDataをLineChartにセット
+        lineChart.setData(data);
+
+        //説明分
+        lineChart.setDescription("体重の遷移");
+
+        //背景色
+        lineChart.setBackgroundColor(Color.WHITE);
+
+        //アニメーション
+        lineChart.animateX(1200);
+
     }
-
-
+}
