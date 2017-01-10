0bd116b1557910074fa02ab5ba66b06ad9c0103d
ykunihiro <Yuto@YutoMac.local>
2017-01-10 21:06:50 +0900

    グラフの見た目完成？詳細はLINEの通り

--- app/src/main/java/com/apuritobokuto/healmane/cl_healthManage.java
+++ app/src/main/java/com/apuritobokuto/healmane/cl_healthManage.java
@@ -11,44 +11,85 @@ import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 
+
 public class cl_healthManage extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
 
-        LineChart lineChart = (LineChart)findViewById(R.id.chart1);
+        //グラフのインスタンス
+        LineChart RLineChart = (LineChart)findViewById(R.id.RChart);
+        LineChart GLineChart = (LineChart)findViewById(R.id.GChart);
+        LineChart YLineChart = (LineChart)findViewById(R.id.YChart);
 
-        //グラフ用データ
-        ArrayList<Entry> entries = new ArrayList<Entry>();
-        entries.add(new Entry(60f,0));
-        entries.add(new Entry(50f,1));
-        entries.add(new Entry(58f,2));
-        //entries.add(new Entry(60f,3));
-        //entries.add(new Entry(65f,4));
-        //entries.add(new Entry(80f,5));
-        //entries.add(new Entry(78f,6));
+        //グラフの右側にy軸を表示しない
+        RLineChart.getAxisRight().setEnabled(false);
+        GLineChart.getAxisRight().setEnabled(false);
+        YLineChart.getAxisRight().setEnabled(false);
+
+        //グラフ用データ(赤)
+        ArrayList<Entry> RedEntries = new ArrayList<Entry>();
+        RedEntries.add(new Entry(60f,0));
+        RedEntries.add(new Entry(50f,1));
+        RedEntries.add(new Entry(58f,2));
+        RedEntries.add(new Entry(23f,3));
+        RedEntries.add(new Entry(56f,4));
+        RedEntries.add(new Entry(90f,5));
+        RedEntries.add(new Entry(13f,6));
+
+        //グラフ用データ(緑)
+        ArrayList<Entry> GreenEntries = new ArrayList<Entry>();
+        GreenEntries.add(new Entry(60f,0));
+        GreenEntries.add(new Entry(50f,1));
+        GreenEntries.add(new Entry(58f,2));
+        GreenEntries.add(new Entry(62f,3));
+        GreenEntries.add(new Entry(66f,4));
+        GreenEntries.add(new Entry(80f,5));
+        GreenEntries.add(new Entry(43f,6));
+
+        //グラフ用データ(黄)
+        ArrayList<Entry> YellowEntries = new ArrayList<Entry>();
+        YellowEntries.add(new Entry(60f,0));
+        YellowEntries.add(new Entry(50f,1));
+        YellowEntries.add(new Entry(58f,2));
+        YellowEntries.add(new Entry(45f,3));
+        YellowEntries.add(new Entry(65f,4));
+        YellowEntries.add(new Entry(67f,5));
+        YellowEntries.add(new Entry(78f,6));
 
         //データをセット
-        LineDataSet dataSet = new LineDataSet(entries,"weight");
+        LineDataSet RedDataSet = new LineDataSet(RedEntries,"赤");
+        LineDataSet GreenDataSet = new LineDataSet(GreenEntries,"緑");
+        LineDataSet YellowDataSet = new LineDataSet(YellowEntries, "黄");
+
+        //線の色付け
+        RedDataSet.setColor(Color.RED);
+        GreenDataSet.setColor(Color.GREEN);
+        YellowDataSet.setColor(Color.YELLOW);
 
         //ラベル
-        String[] labels = {"2015","2016","2017"};
+        String[] labels = {"1日","2日","3日", "4日", "5日", "6日", "7日"};
 
         //LineDataインスタンス生成
-        LineData data = new LineData(labels,dataSet);
+        LineData RedData = new LineData(labels,RedDataSet);
+        LineData GreenData = new LineData(labels, GreenDataSet);
+        LineData YellowData = new LineData(labels,YellowDataSet);
 
-        //LineDataをLineChartにセット
-        lineChart.setData(data);
+                //LineDataをLineChartにセット
+        RLineChart.setData(RedData);
+        GLineChart.setData(GreenData);
+        YLineChart.setData(YellowData);
 
         //説明分
-        lineChart.setDescription("体重の遷移");
+        RLineChart.setDescription("栄養素");
+        GLineChart.setDescription("栄養素");
+        YLineChart.setDescription("栄養素");
 
         //背景色
-        lineChart.setBackgroundColor(Color.WHITE);
-
-        //アニメーション
-        lineChart.animateX(1200);
-
+        RLineChart.setBackgroundColor(Color.WHITE);
+        GLineChart.setBackgroundColor(Color.WHITE);
+        YLineChart.setBackgroundColor(Color.WHITE);
     }
 }
